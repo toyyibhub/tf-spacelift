e85ebf17a78b1000715e955e3c16af0bf4a4567e
Toyyib Salaudeen <toyyib@Toyyibs-MBP.lan>
2024-09-13 16:01:20 -0500

    adding tf files

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = "toyyib-spacelift-001"
+
+  tags = {
+    Name        = "toyyib-spacelift-001"
+    Environment = var.environment
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "The AWS region where the S3 bucket will be created."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket."
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment tag for the S3 bucket (e.g., dev, prod)."
+  type        = string
+  default     = "dev"
+}
